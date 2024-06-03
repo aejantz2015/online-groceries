ea24b18e19f85126f0177989578b7d3c3f581c3f
annajantzen <aejantz2015@gmail.com>
2024-06-03 10:25:47 -0500

    ian helped cleanup the files, should be working on render

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/online-groceries');
+mongoose.connect(process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost:27017/groceries");
 
 module.exports = mongoose.connection;
